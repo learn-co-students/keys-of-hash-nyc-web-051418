32d07b5edc00b97b6125b54bd630332ebe9df37b
Matt Zuzolo <mattzuzolo@outlook.com>
2018-05-01 21:33:46 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Hash
   def keys_of(*arguments)
     
-    arguments.map { |key, value| if value == arguments ? key : nil}
+    arguments.map { |key, value| if arguements.include?(value) ? key : nil}
     
     
     #return an array with every key from the hash whose value matches the value(s) given as an argument.
