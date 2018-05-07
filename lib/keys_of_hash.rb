681c13defb655ffd35171c872304de78aaad76c7
Kayla Fitzgerald <fitzgerald.km11@gmail.com>
2018-05-07 08:55:12 -0400

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,22 @@
+require "pry"
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    # arguments: String
+    # return must be an array
+    # create empty array to store animals where habitat = arguments
+    # iterate through the animals
+    # where value habitat = arguments
+      # push key into initialized array
+    #return array
+
+    habitats = []
+    arguments.each do |hab|
+      self.each do |animal, habitat|
+        if habitat == hab
+          habitats << animal
+        end
+      end
+    end
+    habitats
   end
 end
