5a7c5a1ac3031e0d833257c78aad6e53882cb0c7
Tony Ho <ho.tonym@gmail.com>
2018-05-13 05:45:54 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,22 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+
+
+    args = arguments.map do |element|
+       element
+    end
+
+    matches = []
+    # for every hash
+    self.map do |key, value|
+      args.map do |y|
+        if value == y
+           matches << key
+        end
+      end
+    end
+  return matches
   end
 end
